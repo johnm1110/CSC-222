85ec56dac664f1160f4a9c521948620ac7cdfff3
johnm1110 <jamiller1110@gmail.com>
2025-10-13 11:34:51 -0400

    rock, paper, scissors project

---
+++ rock-paper-scissors.cpp
@@ -0,0 +1,13 @@
+// rock-paper-scissors.cpp : A program to play rock, paper, scissors against the computer
+
+#include <iostream>
+#include <random>
+using namespace std;
+
+int promptHuman();
+int promptComputer();
+string winner(int choiceHuman, choiceComputer)
+void score(int &scoreCOmputer, int &scoreHuman, winner)
+
+int main()
+{
