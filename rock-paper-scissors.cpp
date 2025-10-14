30575477c8cef7586a7290b56928dee805d47121
johnm1110 <jamiller1110@gmail.com>
2025-10-14 11:29:08 -0400

    Update rock-paper-scissors.cpp
    fix syntax errors

--- rock-paper-scissors.cpp
+++ rock-paper-scissors.cpp
@@ -5,22 +5,24 @@
 #include <random>
 using namespace std;
 
-int promptHuman();
-int promptComputer();
-string winner(int, int);
-void score(int, int, string);
+int promptHuman ();
+int promptComputer ();
+string winner (int, int);
+void score (int &, int &, string);
 
 int main ()
 {
     int scoreComputer, scoreHuman, choiceComputer, choiceHuman = 0;
     string roundWinner;
 
-    while (choiceHuman <> 4)
+
+
+    while (choiceHuman != 4)
     {
-		choiceHuman = promptHuman();
-		choiceComputer = promptComputer();
-		roundWinner = winner(choiceHuman, choiceComputer);
-		score(scoreComputer, scoreHuman, roundWinner);
+		choiceHuman = promptHuman ();
+		choiceComputer = promptComputer ();
+		roundWinner = winner (choiceHuman, choiceComputer);
+		score (scoreComputer, scoreHuman, roundWinner);
 
 		if (roundWinner == "computer")
 			cout << choiceComputer << " beats " << choiceHuman << " , the computer wins.";
@@ -29,16 +31,16 @@ int main ()
 		else
 			cout << "You both chose " << choiceHuman << " , draw!";
 
-		cout << "Current score: Computer " << scoreComputer  << ", you << " scoreHuman.";
+		cout << "Current score: Computer " << scoreComputer  << ", you " << scoreHuman;
     }
-return 0
+	return 0;
 }
 
 int promptHuman ()
 {
 	string inputHuman;
 	int inputHumanNum = 4;
-	while (inputHumanNum < 1 and inputHuman > 4)
+	while (inputHumanNum < 1 || inputHumanNum > 4)
 	{
 		cout << "Please choose rock, paper, scissors, or quit";
 		cin >> inputHuman;
@@ -53,41 +55,53 @@ int promptHuman ()
 		else
 			cout << "That is not a correct entry, please try again";
 	}
-	return inputHuman;
+	return inputHumanNum;
 }
 
 int promptComputer ()
 {
-	random_device engine
-		uniform_int_distribution<int> computerInput(MIN, MAX);
-	return computerInput
+
+	const int MIN = 1; // minimum value for the computer's choice
+	const int MAX = 3; // maximum value for the computer's choice
+	int computerInput;
+
+	random_device engine;
+	uniform_int_distribution<int> computerInput(MIN, MAX);
+	return computerInput;
 }
 
 string winner (int choiceHuman, int choiceComputer)
 {
 	string whoWins;
+	
 	if (choiceComputer == 1)
 	{
 		if (choiceHuman == 1)
-			whoWins = draw;
+			whoWins = "draw";
 		else if (choiceHuman == 2)
-			whoWins = human;
-		else (choiceHuman == 3)
-			whoWins = computer;
-	else if (choiceComputer == 2)
-		if (choiceHuman == 1
-			whoWins = computer;
+			whoWins = "human";
+		else 
+			whoWins = "computer";
+	}
+
+	if (choiceComputer == 2)
+	{
+		if (choiceHuman == 1)
+			whoWins = "computer";
 		else if (choiceHuman == 2)
-			whoWins = draw;
-		else (choiceHuman == 3)
-			whoWins = human;
-	else (choiceComputer == 3)
-		else if (choiceHuman == 1)
-			whoWins = human;
+			whoWins = "draw";
+		else 
+			whoWins = "human";
+	}
+
+	if (choiceComputer == 3)
+	{
+		if (choiceHuman == 1)
+			whoWins = "human";
 		else if (choiceHuman == 2)
-			whoWins = computer;
-		else (choiceHuman == 3)
-			whoWins = draw;
+			whoWins = "computer";
+		else 
+			whoWins = "draw";
 	}
 	return whoWins;
 }
@@ -95,8 +109,8 @@ string winner (int choiceHuman, int choiceComputer)
 void score (int &scoreComputer, int &scoreHuman, string winner)
 {
 	if (winner == "computer")
-		scoreComputer is scoreComputer + 1;
+		scoreComputer = scoreComputer + 1;
 	else if (winner == "human")
-		scoreHuman is scoreHuman + 1;
-	return 0;
+		scoreHuman = scoreHuman + 1;
+	return;
 }
