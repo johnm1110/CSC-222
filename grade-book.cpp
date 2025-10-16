5f24fdc97f6aea2aef7ef2f6606fcc574afb6463
johnm1110 <jamiller1110@gmail.com>
2025-10-16 15:30:02 -0400

    initial program

---
+++ grade-book.cpp
@@ -0,0 +1,25 @@
+// grade-book.cpp : a program to read student's grades from a file and output a corresponding letter grade  
+
+
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+main()
+{
+
+	ifstream inputFile;
+	inputFile.open("StudentGrades.txt");
+
+
+
+	inputFile.close();
+
+
+
+
+	return;
+}
