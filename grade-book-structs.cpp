4284faa74e54171b78603fc764ceb7773f1161d8
johnm1110 <jamiller1110@gmail.com>
2025-10-24 20:16:39 -0400

    FInal program

--- grade-book-structs.cpp
+++ grade-book-structs.cpp
@@ -8,79 +8,91 @@
 
 using namespace std;
 
-// Global constants
-const int TESTNUMBER = 5;
+// Define a structure to hold student records
+struct studentRecord
+{
+	string name;			// Student's name
+	int studentID;			// Student's ID
+	double* testScores;		// Pointer to dynamically allocated array of test scores
+	double testAverage;		// Average test score
+};
 
 // Function prototypes
-// Reads student names and their test scores from a file
-// takes the file stream, an array for names, and a 2D array for scores as arguments, returns the number of students read
-int  getGrades(ifstream &, string [], int [][TESTNUMBER]);
+// Reads student names, student ID, and their test scores from a file
+// takes the file stream, pointer to the number of students and number of test scores as arguments
+// returns a pointer to an array of studentRecord structs
+studentRecord* getData(ifstream &, int &, int &);
 
 // Calculates the average test score for each student
-// takes the number of students, a 2D array of scores, and an array for averages as arguments, returns nothing
-void calculateAverage(int, int[][TESTNUMBER], double[]);
+// takes an array of studentRecord structs, number of students, and number of test scores as arguments, returns nothing
+void calculateAverage(studentRecord[], int, int);
 
 // Determines the letter grade based on the average score
-// takes the average score as an argument, returns the corresponding letter grade, returns the letter grade
+// takes the average score as an argument, returns the corresponding letter grade
 char calculateLetterGrade(double);
 
-// Outputs the student names, their average scores, and letter grades
-// takes the number of students, an array of names, and an array of averages as arguments, returns nothing
-void report(int, string[], double[]);
-
+// Outputs the student names, student ID, their average scores, and letter grades
+// takes an array of studentRecord structs and number of students as arguments, returns nothing
+void report(studentRecord[], int);
 
 int main()
 {
+	ifstream file;
+	
+	int studentCnt, testsCnt;
+	studentRecord *students = nullptr; // Pointer to hold the student records
+		
+	file.open("student_data.txt"); // open the input file
 
-	ifstream inputFile;
-	
-	int numberOfStudents, testScores[20][TESTNUMBER];
-	string studentNames[20];
-	double testAverages[20];
-	
-	inputFile.open("StudentGrades.txt");
-	if (!inputFile)
+	if (file)
+	{
+		students = getData(file, studentCnt, testsCnt); // Read data from the file and populate the student records
+	}
+	else
 	{
 		cout << "Error opening input file." << endl;
 		return 0;
 	}
 
-	numberOfStudents = getGrades(inputFile, studentNames, testScores); // Read student names and scores from the file, returns the number of students read
-	inputFile.close(); // Close the file after reading
-	calculateAverage(numberOfStudents, testScores, testAverages); // Calculate the average scores for each student
-	report(numberOfStudents, studentNames, testAverages); // Output the report of student names, average scores, and letter grades
+	file.close(); // Close the file after reading
+
+	calculateAverage(students, studentCnt, testsCnt); // Calculate the average scores for each student
+	report(students, studentCnt); // Output the report of student names, average scores, and letter grades
+	
+	delete[] students; // Free the dynamically allocated memory
 
 	return 0;
 }
 
-int getGrades(ifstream& inFile, string names[], int scores[][TESTNUMBER])
+studentRecord* getData(ifstream &file, int &studentCnt, int &testsCnt)
 {
-	int numberOfRecords = 0; // Initialize the record counter
-
-	while (!inFile.eof()) // Read until the end of the file
+	file >> studentCnt;
+	file >> testsCnt;
+	studentRecord *students = new studentRecord[studentCnt]; // Dynamically allocate memory for student records
+	for (int i = 0; i < studentCnt; i++)
 	{
-		inFile >> names[numberOfRecords];
-		for (int j = 0; j < TESTNUMBER; j++)
+		file >> students[i].name; // Read the student's name
+		file >> students[i].studentID; // Read the student's ID
+		students[i].testScores = new double[testsCnt]; // Dynamically allocate memory for test scores
+		for (int j = 0; j < testsCnt; j++)
 		{
-			inFile >> scores[numberOfRecords][j]; // Read each test score for the student
+			file >> students[i].testScores[j]; // Read each test score for the student
 		}
-		numberOfRecords++; //
 	}
-	return (numberOfRecords - 1); // Return the total number of records read, adjusting for the extra increment after the last read
-	
+	return (students); // pointer to student records
 }
 
-void calculateAverage(int numberOfRecords, int scores[][TESTNUMBER], double averages[])
+void calculateAverage(studentRecord students[], int studentCnt, int testsCnt)
 {
 	double sum = 0; // Variable to hold the sum of scores for each student
 
-	for (int i = 0; i < numberOfRecords; i++)
+	for (int i = 0; i < studentCnt; i++)
 	{
-		for (int j = 0; j < TESTNUMBER; j++)
+		for (int j = 0; j < testsCnt; j++)
 		{
-			sum = sum + scores[i][j]; // Sum the scores for the current student
+			sum = sum + students[i].testScores[j]; // Sum the scores for the current student
 		}
-		averages[i] = sum / TESTNUMBER; //
+		students[i].testAverage = sum / testsCnt; // Calculate the average score for the current student
 		sum = 0; // Reset sum for the next student
 
 	}
@@ -102,15 +114,18 @@ char calculateLetterGrade(double average)
 
 }
 
-void report(int numberofRecords, string names[], double averages[])
+void report(studentRecord students[], int studentCnt)
 {
 	cout << "Student Grade Report" << endl;
-	cout <<left << setw(15) << "Student Name  " << setw(15) << "Average  " << "Letter Grade" << endl;
+	cout << left << setw(15) << "Student Name" << setw(15) << "Student ID" << setw(15) << "Average" << "Letter Grade" << endl;
 
-	for (int i = 0; i < numberofRecords; i++)
+	for (int i = 0; i < studentCnt; i++)
 	{
-		cout << left << setw(15) << names[i] << setw(15) << setprecision(0) << fixed << averages[i] << setw(10) << calculateLetterGrade(averages[i]) << endl;
+		cout << left 
+			<< setw(15) << students[i].name 
+			<< setw(15) << students[i].studentID 
+			<< setw(15) << setprecision(1) << fixed << students[i].testAverage 
+			<< calculateLetterGrade(students[i].testAverage) 
+			<< endl;
 	}
-
-	return;
 }
